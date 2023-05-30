607d3a110c963a5bb45d0c110284ba0fb98fea4f
Igor Bandeira <igorbandeirasouza03@gmail.com>
2023-05-30 19:22:18 -0300

    AudioChat

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Microfone from "./components/Microfone";
+
+function App() {
+  return (
+    <div>
+      <Microfone />
+    </div>
+  );
+}
+
+export default App;
